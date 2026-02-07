31a4d7a0107a15c92289a1be1e503b350a6f28cc
2834 <2682423431@qq.com>
2026-02-07 21:17:45 +0800

    feat(agent): 添加会话历史记录功能
    添加了langgraph检查点支持以实现对话历史记录功能，
    修改ReactAgent类构造函数以接受checkpoint参数，
    更新excute_stream方法以支持session配置，
    在配置文件中添加历史记录类型选项，
    支持InMemoryHistory类型的会话持久化。

--- agent.py
+++ agent.py
@@ -5,27 +5,29 @@ from utils.prompt_loader import load_prompt
 from utils.logger import get_logger
 from tools.tools import get_rag, get_weather, get_time, get_weather_by_city, get_emoji, check_file, check_filelist, modify_file
 from tools.middleware import monitor_tool_call, monitor_model
+from langgraph.checkpoint.memory import InMemorySaver
 
 logger = get_logger("Agent")
 
 class ReactAgent:
-    def __init__(self):
+    def __init__(self, checkpoint):
         super().__init__()
         self.agent = create_agent(
             model= OllamaChatModel().generator(config["models"]["chat_model"]),
             tools= [get_rag, get_time, get_weather, get_weather_by_city, get_emoji, check_file, check_filelist, modify_file],
             middleware= [monitor_tool_call, monitor_model],
-            checkpointer= None,
+            checkpointer= checkpoint,
             system_prompt= load_prompt(config["prompts"]["system_prompt"]),
         )
         
-    def excute_stream(self, inputs: str):
+    def excute_stream(self, inputs: str, session_config: dict):
         stream = self.agent.stream(
             {
                 "messages": [
                     {"role": "user", "content": inputs}
                 ]
             },
+            session_config, # pyright: ignore[reportArgumentType]
             stream_mode= "values"
         )
         
@@ -38,10 +40,22 @@ class ReactAgent:
 
 
 if __name__ == '__main__':
-    agent = ReactAgent()
+    
+    checkpoint = None
+    session_config = {
+        "configurable":{
+            "thread_id": "1"
+        }
+    }
+    
+    if config["history"]["type"] == "InMemoryHistory":
+        checkpoint = InMemorySaver()
+        
+    agent = ReactAgent(checkpoint)
+    
     while True:
         inputs = input("> ")
         if inputs == "exit":
             break
-        for chunk in agent.excute_stream(inputs):
+        for chunk in agent.excute_stream(inputs, session_config):
             print(chunk, end= "", flush= True)
